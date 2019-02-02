216743c2e731d7a916e4ef924a8cedfb8ea533ec
CaspianBorder <970558937@qq.com>
2019-02-02 13:05:46 +0800

    hotfix: SpringbootWebApplicationTests No runnable methods

--- backend/springboot-web/src/test/java/com/spbweb/SpringbootWebApplicationTests.java
+++ backend/springboot-web/src/test/java/com/spbweb/SpringbootWebApplicationTests.java
@@ -1,5 +1,6 @@
 package com.spbweb;
 
+import org.junit.Test ;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,5 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootWebApplicationTests
 {
+    @Test
+    public void contextLoads() //to be added
+    {
+    }
 }
 
